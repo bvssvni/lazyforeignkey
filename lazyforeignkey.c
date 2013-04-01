c76d40493c59f4329e51fa4c4fe6ff26746c15dd
Sven Nilsen <sven@cutoutpro.com>
2013-04-01 03:44:48 +0200

    Added reading from file.

--- lazyforeignkey.c
+++ lazyforeignkey.c
@@ -4,7 +4,16 @@
 #include <string.h>
 #include "lazyforeignkey.h"
 
-void LFK_LookUp(LFK_Table const *table, LFK_ForeignKey *key) {
+LFK_ForeignKey LFK_AddRow(LFK_Table *table) {
+	long id = table->id_counter;
+	int pos = table->len;
+	table->ids[pos] = id;
+	table->id_counter++;
+	table->len++;
+	return (LFK_ForeignKey){.id = id, .llup = pos};
+}
+
+void LFK_LookUp(const LFK_Table *table, LFK_ForeignKey *key) {
 	if (key->id == -1L) return;
 	
 	int llup = key->llup == -1 ? table->len - 1 : key->llup;
@@ -26,7 +35,7 @@ void LFK_LookUp(LFK_Table const *table, LFK_ForeignKey *key) {
 	return;
 }
 
-LFK_column LFK_AddColumn(LFK_Table *table, int item_size) {
+int LFK_AddColumn(LFK_Table *table, int item_size) {
 	int id = table->column_len;
 	table->column_len++;
 	table->column_size = realloc(table->column_size,
@@ -36,10 +45,10 @@ LFK_column LFK_AddColumn(LFK_Table *table, int item_size) {
 	table->data[id] = malloc(item_size * table->len);
 	table->column_size[id] = item_size;
 	memset(table->data[id], 0, item_size * table->len);
-	return (LFK_column){id};
+	return id;
 }
 
-LFK_key LFK_AddForeignKey(LFK_Table *table) {
+int LFK_AddForeignKey(LFK_Table *table) {
 	int id = table->foreignkey_len;
 	table->foreignkey_len++;
 	table->foreignkey_values = realloc
@@ -47,7 +56,7 @@ LFK_key LFK_AddForeignKey(LFK_Table *table) {
 	 sizeof(*table->foreignkey_values) * table->foreignkey_len);
 	table->foreignkey_values[id] = malloc(sizeof(double) * table->cap);
 	memset(table->foreignkey_values[id], 0, sizeof(double) * table->cap);
-	return (LFK_key){id};
+	return id;
 }
 
 
@@ -107,19 +116,65 @@ void LFK_Write(const LFK_Table *table, FILE *f) {
 	}
 }
 
-void LFK_FreeTable(LFK_Table *table) {
+LFK_Table *LFK_Read(FILE *f) {
+	int len, cap;
+	fread(&len, sizeof(len), 1, f);
+	fread(&cap, sizeof(cap), 1, f);
+	LFK_Table *table = LFK_NewTable(cap);
+	table->len = len;
+	fread(&table->id_counter, sizeof(table->id_counter), 1, f);
+	fread(table->ids, sizeof(*table->ids), table->len, f);
+	fread(&table->column_len, sizeof(table->column_len), 1, f);
+	// Allocate array that tells size of columns.
+	if (table->column_len > 0) {
+		table->column_size = malloc
+		(sizeof(*table->column_size) * table->column_len);
+	}
+		
+	fread(table->column_size, sizeof(*table->column_size), table->column_len, f);
+	// Allocate array that contains data.
+	if (table->cap > 0) {
+		table->data = malloc(sizeof(*table->data) * table->cap);
+	}
+		
 	int i;
 	for (i = 0; i < table->column_len; i++) {
-		free(table->data[i]);
-	}
-	for (i = 0; i < table->foreignkey_len; i++) {
-		free(table->foreignkey_values[i]);
+		// Allocate data for column.
+		if (table->cap > 0) {
+			table->data[i] = malloc(table->column_size[i] * table->cap);
+		}
+		
+		fread(table->data[i], table->column_size[i], table->len, f);
 	}
 	
-	free(table->column_size);
-	free(table->data);
-	free(table->foreignkey_values);
-	free(table->ids);
+	fread(&table->foreignkey_len, sizeof(table->foreignkey_len), 1, f);
+	// Allocate array that contains foreign keys.
+	if (table->foreignkey_len > 0) {
+		table->foreignkey_values = malloc
+		(sizeof(*table->foreignkey_values) * table->foreignkey_len);
+	}
+		
+	for (i = 0; i < table->foreignkey_len; i++) {
+		fread(table->foreignkey_values[i], sizeof(LFK_ForeignKey), table->len, f);
+	}
+	
+	return table;
+}
+
+void LFK_FreeTable(LFK_Table *const table) {
+	int i;
+	for (i = 0; i < table->column_len; i++) {
+		if (table->data[i] != NULL) free(table->data[i]);
+	}
+
+	for (i = 0; i < table->foreignkey_len; i++) {
+		if (table->foreignkey_values[i] != NULL) free(table->foreignkey_values[i]);
+	}
+
+	if (table->column_size != NULL) free(table->column_size);
+	if (table->data != NULL) free(table->data);
+	if (table->foreignkey_values != NULL) free(table->foreignkey_values);
+	if (table->ids != NULL) free(table->ids);
 	
 	free(table);
 }
--- test-lazyforeignkey.c
+++ test-lazyforeignkey.c
@@ -14,14 +14,18 @@ exit
 #include <assert.h>
 #include "lazyforeignkey.h"
 
-long InsertRow(LFK_Table *table, int val) {
-	long id = table->id_counter;
-	int pos = table->len;
-	table->ids[pos] = id;
-	((int*)table->data[0])[pos] = val;
-	table->id_counter++;
-	table->len++;
-	return id;
+typedef struct {
+	LFK_Table *table;
+} People;
+
+typedef struct {
+	long id;
+} Person;
+
+Person InsertPerson(People people, int val) {
+	LFK_ForeignKey key = LFK_AddRow(people.table);
+	((int*)people.table->data[0])[key.llup] = val;
+	return (Person){key.id};
 }
 
 void DeleteRow(LFK_Table *table, int pos) {
@@ -33,53 +37,93 @@ LFK_ForeignKey LFK_CreateForeignKey(long id) {
 }
 
 void TestDefragmentation(void) {
-	LFK_Table *people = LFK_NewTable(100);
-	LFK_AddColumn(people, sizeof(int));
+	People people = (People){LFK_NewTable(100)};
+	LFK_AddColumn(people.table, sizeof(int));
 	
 	int i;
 	int cl = 1 << 0;
 	for (i = 0; i < cl; i++) {
 	
-		long carl = InsertRow(people, 3);
-		LFK_ForeignKey carl_key = LFK_CreateForeignKey(carl);
-		LFK_LookUp(people, &carl_key);
+		Person carl = InsertPerson(people, 3);
+		LFK_ForeignKey carl_key = LFK_CreateForeignKey(carl.id);
+		LFK_LookUp(people.table, &carl_key);
 		
 		assert(carl_key.llup == 0);
-		DeleteRow(people, 0);
-		LFK_LookUp(people, &carl_key);
+		DeleteRow(people.table, 0);
+		LFK_LookUp(people.table, &carl_key);
 		assert(carl_key.id == -1L);
 		
-		long john = InsertRow(people, 5);
-		long sara = InsertRow(people, 7);
+		Person john = InsertPerson(people, 5);
+		Person sara = InsertPerson(people, 7);
 		
-		LFK_ForeignKey john_key = LFK_CreateForeignKey(john);
-		LFK_LookUp(people, &john_key);
+		LFK_ForeignKey john_key = LFK_CreateForeignKey(john.id);
+		LFK_LookUp(people.table, &john_key);
 		assert(john_key.llup == 1);
-		LFK_ForeignKey sara_key = LFK_CreateForeignKey(sara);
-		LFK_LookUp(people, &sara_key);
+		LFK_ForeignKey sara_key = LFK_CreateForeignKey(sara.id);
+		LFK_LookUp(people.table, &sara_key);
 		assert(sara_key.llup == 2);
 		
-		LFK_Defragment(people);
+		LFK_Defragment(people.table);
 		
-		LFK_LookUp(people, &john_key);
+		LFK_LookUp(people.table, &john_key);
 		assert(john_key.llup == 0);
-		LFK_LookUp(people, &sara_key);
+		LFK_LookUp(people.table, &sara_key);
 		assert(sara_key.llup == 1);
 		
-		DeleteRow(people, 0);
-		DeleteRow(people, 1);
+		DeleteRow(people.table, 0);
+		DeleteRow(people.table, 1);
 		
-		LFK_Defragment(people);
+		LFK_Defragment(people.table);
 	}
 	
-	LFK_FreeTable(people);
+	LFK_FreeTable(people.table);
+}
+
+typedef struct {
+	LFK_Table *table;
+} Vectors;
+
+typedef struct {
+	int id;
+} Vector;
+
+Vector InsertVector(Vectors v, float x, float y) {
+	LFK_ForeignKey key = LFK_AddRow(v.table);
+	((float*)v.table->data[0])[key.llup] = x;
+	((float*)v.table->data[1])[key.llup] = y;
+	return (Vector){key.id};
+}
+
+float FloatValue(LFK_Table *table, int col, int row) {
+	return ((float*)table->data[col])[row];
 }
 
 void TestReadWrite(void) {
-	LFK_Table *const t = LFK_NewTable(4);
-	FILE *f = fopen("test.bin", "w");
-	LFK_Write(t, f);
+	Vectors t = (Vectors){LFK_NewTable(4)};
+	LFK_AddColumn(t.table, sizeof(float));
+	LFK_AddColumn(t.table, sizeof(float));
+	InsertVector(t, 10, 20);
+	InsertVector(t, 11, 21);
+	
+	const char* file = "test.bin";
+	FILE *f = fopen(file, "w");
+	LFK_Write(t.table, f);
 	fclose(f);
+	
+	LFK_FreeTable(t.table);
+	
+	f = fopen(file, "r");
+	Vectors t2 = (Vectors){LFK_Read(f)};
+	fclose(f);
+	
+	assert(t2.table->column_len == 2);
+	assert(t2.table != NULL);
+	assert(FloatValue(t2.table, 0, 0) == 10);
+	assert(FloatValue(t2.table, 1, 0) == 20);
+	assert(FloatValue(t2.table, 0, 1) == 11);
+	assert(FloatValue(t2.table, 1, 1) == 21);
+
+	LFK_FreeTable(t2.table);
 }
 
 int main(int argc, char *argv[]) {
